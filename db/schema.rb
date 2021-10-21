ce5ed4173db349c51ceafd19068067528a494115
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-10-22 00:58:25 +0530

    Added association with models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_185726) do
+ActiveRecord::Schema.define(version: 2021_10_21_192253) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
