33db38acfb4b272f14c9924efaee2921ad5c173d
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-10-22 02:22:27 +0530

    Added Comment Edit, Update and Delete Functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
 class CommentsController < ApplicationController
 
+    before_action :find_message, only: [:create, :update, :edit, :destroy]
+    before_action :find_comment, only: [:update, :edit, :destroy]
+
     def create
         @message =Message.find(params[:message_id])
         @comment =@message.comments.create(comment_params)
@@ -12,9 +15,34 @@ class CommentsController < ApplicationController
         end
     end
 
+    def edit       #Only responsible for view file
+    end
+
+    def update     # repsponsible for update in database
+      if @comment.update(comment_params)
+        redirect_to message_path(@message)
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @comment.destroy
+      redirect_to message_path(@message)
+    end
+
     private
 
     def comment_params 
         params.require(:comment).permit(:comment)
     end
+
+    def find_message
+      @message = Message.find(params[:message_id])
+    end
+
+    def find_comment
+      @comment = @message.comments.find(params[:id])
+    end
+
 end
