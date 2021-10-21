9264073549f1adbc36249ac6043c66b8218ed02c
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-10-22 01:16:24 +0530

    Added comments to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ApplicationRecord
     belongs_to :user
+    has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_192253) do
+ActiveRecord::Schema.define(version: 2021_10_21_193230) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "message_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["message_id"], name: "index_comments_on_message_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -32,4 +42,6 @@ ActiveRecord::Schema.define(version: 2021_10_21_192253) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "messages"
+  add_foreign_key "comments", "users"
 end
